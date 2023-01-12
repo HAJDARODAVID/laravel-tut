ad8e31c9365ddb55b465abf7786a3f37cd65195b
Hajdarovic David <david.hajdarovic@boxmark.com>
2023-01-12 08:53:49 +0100

    prvi dio druge lekcije

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+//Route::get('/blog', [PostsController::class, 'index']);
 
-    return view('welcome');
-});
+Route::resource('blog', PostsController::class);
