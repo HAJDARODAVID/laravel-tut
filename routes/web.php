9bbac284dcdfc923acce4b97e555faacd04d600a
Hajdarovic David <david.hajdarovic@boxmark.com>
2023-01-12 09:17:44 +0100

    routes lessons

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/blog', [PostsController::class, 'index']);
+/**
+ * GET - Request a resource
+ * POST - create a new resource
+ * PUT - Update a resource
+ * PATCH - Modify a resource
+ * DELETE - Delete a resource
+ * OPTIONS - Ask the server which verbs are allowed
+ */
 
-Route::resource('blog', PostsController::class);
+// GET
+Route::get('/blog', [PostsController::class, 'index']);
+Route::get('/blog/1', [PostsController::class, 'show']);
+
+// POST
+Route::get('/blog/create', [PostsController::class, 'create']);
+Route::post('/blog', [PostsController::class, 'store']);
+
+// PUT or PATCH
+Route::get('/blog/edit/1', [PostsController::class, 'edit']);
+Route::post('/blog/1', [PostsController::class, 'update']);
+
+// DELETE
+Route::delete('/blog/1', [PostsController::class, 'destroy']);
+
+// Multiple HTTP verbs
+//Route::match(['GET', 'POST'], '/blog', [PostsController::class, 'index']);
+//Route::any('/blog', [PostsController::class, 'index']);
+
+// Return view
+// Route::view('/blog', 'blog.index', ['name' => 'Coding with maMan the big D-David']);
+
+//Route::resource('blog', PostsController::class);
+
+//route for the invoke method
+Route::get('/', HomeController::class);
